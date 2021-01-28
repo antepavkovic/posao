f1cac76cc42047e8ccbb1c3544b218eb94e9f833
antepavkovic <anteogame123>
2021-01-28 20:23:43 +0100

    proba

--- opg/main/urls.py
+++ opg/main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns=[
-    url(r'^$',views.home,name='home'),
+    url(r'^$',views.index,name='index'),
     url(r'^login/$', views.login, name='login'),
     url(r'^mylogout/$', views.mylogout, name='mylogout'),
     url(r'^registracija/$', views.registracija, name='registracija'),
